2a21aa775062e19b9775ee9fa2fddf3bb8c5e156
xiongshiji <845056166@qq.com>
2020-09-26 23:29:13 +0800

    身份证生成器

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function getAddrText(text) {
         return {
             input: getFontImg('./fonts/hei.ttf', t, 33),
             left: 335,
-            top: 535 + 48 * i
+            top: 350 + 48 * i
         };
     });
 }
@@ -60,119 +60,118 @@ async function getAvatar(img) {
 }
 
 function composite(config) {
-    return new Promise(async (resolve, reject) => {
-        let test = validateConfig(config);
-        if (!config || test !== undefined) {
-            reject(new Error(`缺少参数：${test}`));
-            return false;
-        };
-        sharp(path.resolve(__dirname, './images/empty.png'))
-            .composite(
-                [
-                    {
-                        // 签发机关
-                        input: getFontImg(
-                            './fonts/hei.ttf',
-                            config.org,
-                            33
-                        ),
-                        left: 525,
-                        top: 1365
-                    },
-                    {
-                        // 有效期限
-                        input: getFontImg(
-                            './fonts/fzzdxjw-gb1-0.ttf',
-                            config.validTerm,
-                            33
-                        ),
-                        left: 525,
-                        top: 1448
-                    },
-                    {
-                        // 身份证号码
-                        input: getFontImg(
-                            './fonts/ocrb10bt.ttf',
-                            config.idn,
-                            44,
-                            0.025
-                        ),
-                        left: 475,
-                        top: 720
-                    },
-                    {
-                        // 姓名
-                        input: getFontImg(
-                            './fonts/hei.ttf',
-                            config.name,
-                            36
-                        ),
-                        left: 340,
-                        top: 325
-                    },
-                    {
-                        // 性别
-                        input: getFontImg(
-                            './fonts/hei.ttf',
-                            config.sex,
-                            33
-                        ),
-                        left: 340,
-                        top: 395
-                    },
-                    {
-                        // 民族
-                        input: getFontImg(
-                            './fonts/hei.ttf',
-                            config.nation,
-                            33
-                        ),
-                        left: 523,
-                        top: 395
-                    },
-                    {
-                        // 年
-                        input: getFontImg(
-                            './fonts/fzzdxjw-gb1-0.ttf',
-                            config.year,
-                            33
-                        ),
-                        left: 340,
-                        top: 473
-                    },
-                    {
-                        // 月
-                        input: getFontImg(
-                            './fonts/fzzdxjw-gb1-0.ttf',
-                            config.mon,
-                            33
-                        ),
-                        left: config.mon.length === 2 ? 475 : 485,
-                        top: 473
-                    },
-                    {
-                        // 日
-                        input: getFontImg(
-                            './fonts/fzzdxjw-gb1-0.ttf',
-                            config.day,
-                            33
-                        ),
-                        left: config.mon.length === 2 ? 580 : 585,
-                        top: 473
-                    },
-                    {
-                        // 头像
-                        input: await getAvatar(config.avatar),
-                        left: 750,
-                        top: 335
-                    }
-                ].concat(getAddrText(config.addr))
-            )
-            .png()
+    const p1 = sharp(path.resolve(__dirname, './images/back.png'))
+        .composite(
+            [
+              {
+                  // 签发机关
+                  input: getFontImg(
+                      './fonts/hei.ttf',
+                      config.org,
+                      33
+                  ),
+                  left: 525,
+                  top: 470
+              },
+              {
+                  // 有效期限
+                  input: getFontImg(
+                      './fonts/fzzdxjw-gb1-0.ttf',
+                      config.validTerm,
+                      33
+                  ),
+                  left: 525,
+                  top: 550
+              }
+            ]
+          ).png()
             .toBuffer()
-            .then(resolve)
-            .catch(reject);
-    });
+      const p2 = new Promise(async (resolve, reject) => {
+        return sharp(path.resolve(__dirname, './images/forward.png'))
+          .composite(
+              [
+                {
+                    // 身份证号码
+                    input: getFontImg(
+                        './fonts/ocrb10bt.ttf',
+                        config.idn,
+                        44,
+                        0.025
+                    ),
+                    left: 475,
+                    top: 535
+                },
+                {
+                    // 姓名
+                    input: getFontImg(
+                        './fonts/hei.ttf',
+                        config.name,
+                        36
+                    ),
+                    left: 340,
+                    top: 140
+                },
+                {
+                    // 性别
+                    input: getFontImg(
+                        './fonts/hei.ttf',
+                        config.sex,
+                        33
+                    ),
+                    left: 340,
+                    top: 210
+                },
+                {
+                    // 民族
+                    input: getFontImg(
+                        './fonts/hei.ttf',
+                        config.nation,
+                        33
+                    ),
+                    left: 523,
+                    top: 210
+                },
+                {
+                    // 年
+                    input: getFontImg(
+                        './fonts/fzzdxjw-gb1-0.ttf',
+                        config.year,
+                        33
+                    ),
+                    left: 340,
+                    top: 288
+                },
+                {
+                    // 月
+                    input: getFontImg(
+                        './fonts/fzzdxjw-gb1-0.ttf',
+                        config.mon,
+                        33
+                    ),
+                    left: config.mon.length === 2 ? 475 : 485,
+                    top: 288
+                },
+                {
+                    // 日
+                    input: getFontImg(
+                        './fonts/fzzdxjw-gb1-0.ttf',
+                        config.day,
+                        33
+                    ),
+                    left: config.mon.length === 2 ? 580 : 585,
+                    top: 288
+                },
+                {
+                    // 头像
+                    input: await getAvatar(config.avatar),
+                    left: 750,
+                    top: 150
+                }
+              ].concat(getAddrText(config.addr))).png()
+              .toBuffer()
+              .then(resolve)
+          })
+      return Promise.all([p1, p2])
 }
 
 module.exports = composite;
--- test.js
+++ test.js
@@ -2,27 +2,54 @@ const idcardGenerator = require('./index');
 const fs = require('fs');
 const path = require('path');
 const config = {
-    name: '刘强东',
-    sex: '女',
+    name: '张三丰',
+    sex: '男',
     nation: '汉',
     year: '2002',
     mon: '2',
     day: '03',
-    org: '喵喵县公安局',
+    org: '深圳市南山区公安局',
     validTerm: '2014.01.27-2019.01.27',
-    addr: '喵喵喵喵喵喵喵喵喵喵喵喵喵喵喵喵喵喵喵喵喵',
+    addr: '深圳市南山区南山街道南园村正街69号',
     idn: '371100197308161831',
     avatar: './images/avatar.png'
 }
 
-idcardGenerator(config).then(e => {
-    fs.writeFile('./output.png', e, err => {
-        if(err){
-            console.log('idcard-generator：测试失败 ' + e);
-        } else {
-            console.log('idcard-generator：测试成功，文件已保存在：' + path.resolve(__dirname, './output.png'));
-        }
+idcardGenerator(config).then(([e1, e2]) => {
+    console.log(e2, e1);
+    const p1 = new Promise((resolve, reject) => {
+      fs.writeFile('./output/back.png', e1, err => {
+          if(err){
+              console.log('idcard-generator：测试失败 ');
+          } else {
+              // console.log('idcard-generator：测试成功，文件已保存在：' + path.resolve(__dirname, './output.png'));
+              resolve();
+          }
+      })
     })
+    const p2 = new Promise((resolve, reject) => {
+      fs.writeFile('./output/foward.png', e2, err => {
+          if(err){
+              console.log(err);
+          } else {
+              // console.log('idcard-generator：测试成功，文件已保存在：' + path.resolve(__dirname, './output.png'));
+              resolve();
+          }
+      })
+    })
+    Promise.all([p1, p2]).then(() => {
+      console.log('idcard-generator：测试成功，文件已保存在：' + path.resolve(__dirname, './output'));
+    }).catch(err => {
+        console.log('idcard-generator：测试失败\n' + err.stack);
+    });
+    //
+    // fs.writeFile('./output.png', e1, err => {
+    //     if(err){
+    //         console.log('idcard-generator：测试失败 ' + e);
+    //     } else {
+    //         console.log('idcard-generator：测试成功，文件已保存在：' + path.resolve(__dirname, './output.png'));
+    //     }
+    // })
 }).catch(err => {
     console.log('idcard-generator：测试失败\n' + err.stack);
 });
